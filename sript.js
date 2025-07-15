af8984cd3f638d41b9125484fd333a5f6d66caf4
PopoolaHakeem <hakeempopoola04@gmail.com>
2025-07-15 14:49:47 +0100

    updated index

--- sript.js
+++ sript.js
@@ -1,11 +1,11 @@
-var MenuButton = document.querySelector('#menu-icon');
-var CancelButton = document.querySelector('#cancel-icon');
-var navlist = document.querySelector('#navbar-list')
+var MenuButton = document.querySelector('.menu-icon');
+var CancelButton = document.querySelector('.cancel-icon');
+var navlist = document.querySelector('.navbar-list')
 
 MenuButton.onclick = function() {
     MenuButton.style.display = 'none'
     CancelButton.style.display = 'block'
-    navlist.style.display = 'none'
+    navlist.style.display = 'block'
 }
 CancelButton.onclick = function() {
     MenuButton.style.display = 'block'
